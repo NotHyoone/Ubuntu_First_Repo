8208d6a1c807f788c171325ff8416f248a4239c5
NotHyooon <gydnjs3505@gmail.com>
2026-05-13 22:17:51 +0900

    refactor: 코드 수정-가독성 좋도록

--- ComputerNetwork/echo_mpserv.c
+++ ComputerNetwork/echo_mpserv.c
@@ -29,7 +29,9 @@ int main(int argc, char *argv[]) {
 	act.sa_handler=read_childproc;
 	sigemptyset(&act.sa_mask);
 	act.sa_flags=0;
+	
 	state=sigaction(SIGCHLD, &act, 0);
+	
 	serv_sock=socket(PF_INET, SOCK_STREAM, 0);
 	memset(&serv_adr, 0, sizeof(serv_adr));
 	serv_adr.sin_family=AF_INET;
@@ -37,23 +39,30 @@ int main(int argc, char *argv[]) {
 	serv_adr.sin_port=htons(atoi(argv[1]));
 
 	if(bind(serv_sock, (struct sockaddr*) &serv_adr, sizeof(serv_adr))==-1)
-		error_handling("bint() error");
-	if(listen(serv_sock, 5)==-1)
+		error_handling("bind() error");
+	
+		if(listen(serv_sock, 5)==-1)
 		error_handling("listen() error");
 
 	while(1){
 		adr_sz=sizeof(clnt_adr);
+		
 		clnt_sock=accept(serv_sock, (struct sockaddr*) &clnt_adr, &adr_sz);
+		
 		if(clnt_sock==-1)
 			continue;
-		else puts("new client connected...");
+		
+			else puts("new client connected...");
 		pid=fork();
+		
 		if(pid==-1){
 			close(clnt_sock);
 			continue;
 		}
+		
 		if(pid==0){
 			close(serv_sock);
+			
 			while( (str_len=read(clnt_sock, buf, BUF_SIZE) )!=0)
 				write(clnt_sock, buf, str_len);
 
@@ -71,7 +80,9 @@ void read_childproc(int sig) {
 
 	pid_t pid;
 	int status;
+	
 	pid=waitpid(-1, &status, WNOHANG);
+	
 	printf("removed proc id: %d\n", pid);
 }
 void error_handling(char *message){
--- ComputerNetwork/waitpid.c
+++ ComputerNetwork/waitpid.c
@@ -7,23 +7,20 @@ int main(int argc, char *argv[])
 	int status;
 	pid_t pid=fork();
 	
-	if(pid==0)
-	{
+	if(pid==0){
 		sleep(5);
 		printf("I'm child process. I'm terminated!!\n");
 		return 24;   	
 	}
-	else
-	{
+	else{
 		printf("Child PID: %d \n",pid);
-		while(!waitpid(-1, &status, WNOHANG))
-		{
+		while(!waitpid(-1, &status, WNOHANG)){
 			sleep(1);
 			puts("sleep 1sec.");
 		}
-			printf("Child Process (PID:%d is terminated",pid);
-			printf("30 sec timer is started!\n");
-			sleep(30);
+		printf("Child Process (PID:%d is terminated",pid);
+		printf("30 sec timer is started!\n");
+		sleep(30);
 
 		if(WIFEXITED(status))
 			printf("Child send %d \n", WEXITSTATUS(status));
